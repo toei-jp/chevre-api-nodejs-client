bc4dc4dec410109f342b6c916fa8b7c87cd0c57c
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-09-17 15:12:04 +0000

    # TOEI-4 ### Added - 取引検索条件インターフェースを追加。

--- example/transaction/processReserve.js
+++ example/transaction/processReserve.js
@@ -22,25 +22,31 @@ const reserveService = new client.service.transaction.Reserve({
 async function main() {
     console.log('searching events...');
     const events = await eventService.searchScreeningEvents({
-        startFrom: new Date(),
-        endThrough: moment().add(1, 'month').toDate()
+        inSessionFrom: new Date(),
+        inSessionThrough: moment().add(1, 'month').toDate()
     });
-    console.log(events.length, 'events found');
+    console.log(events.totalCount, 'events found');
+    const selectedEvent = events.data[0];
 
     console.log('searching ticket types...');
-    const ticketTypes = await eventService.searchScreeningEventTicketTypes({ eventId: events[0].id });
+    const ticketTypes = await eventService.searchScreeningEventTicketTypes({ eventId: selectedEvent.id });
     console.log('ticketTypes:', ticketTypes);
 
     console.log('searching offers...');
-    const offers = await eventService.searchScreeningEventOffers({ eventId: events[0].id });
+    const offers = await eventService.searchScreeningEventOffers({ eventId: selectedEvent.id });
     console.log('offers:', offers);
     const seatOffers = offers[0].containsPlace;
     console.log(seatOffers.length, 'seatOffers found');
     const availableSeatOffers = seatOffers.filter((o) => o.offers[0].availability === client.factory.itemAvailability.InStock);
     console.log(availableSeatOffers.length, 'availableSeatOffers found');
 
+    const selectedSectionOffer = offers[0];
+    const selectedSeatOffer = availableSeatOffers[0];
+    const selectedTicketType = ticketTypes[0];
+    console.log('reserving...', selectedEvent.id, selectedSectionOffer.branchCode, selectedSeatOffer.branchCode, selectedTicketType.id);
+
     console.log('starting transaction...');
-    const transaction = await reserveService.start({
+    let transaction = await reserveService.start({
         typeOf: client.factory.transactionType.Reserve,
         agent: {
             typeOf: 'Person',
@@ -48,16 +54,16 @@ async function main() {
         },
         object: {
             event: {
-                id: events[0].id
+                id: selectedEvent.id
             },
             tickets: [
                 {
                     ticketType: {
-                        id: ticketTypes[0].id
+                        id: selectedTicketType.id
                     },
                     ticketedSeat: {
-                        seatNumber: availableSeatOffers[0].branchCode,
-                        seatSection: offers[0].branchCode
+                        seatNumber: selectedSeatOffer.branchCode,
+                        seatSection: selectedSectionOffer.branchCode
                     }
                 }
             ],
@@ -70,11 +76,45 @@ async function main() {
 
     await wait(1000);
 
-    // 確定
-    const result = await reserveService.confirm({
+    // 中止
+    await reserveService.cancel({
         transactionId: transaction.id
     });
-    console.log('transaction confirmed', result);
+    console.log('transaction canceled');
+
+    transaction = await reserveService.start({
+        typeOf: client.factory.transactionType.Reserve,
+        agent: {
+            typeOf: 'Person',
+            name: 'agent name'
+        },
+        object: {
+            event: {
+                id: selectedEvent.id
+            },
+            tickets: [
+                {
+                    ticketType: {
+                        id: selectedTicketType.id
+                    },
+                    ticketedSeat: {
+                        seatNumber: selectedSeatOffer.branchCode,
+                        seatSection: selectedSectionOffer.branchCode
+                    }
+                }
+            ],
+            notes: 'test from samples'
+
+        },
+        expires: moment().add(5, 'minutes').toDate()
+    });
+    console.log('transaction started', transaction.id);
+
+    // 確定
+    await reserveService.confirm({
+        transactionId: transaction.id
+    });
+    console.log('transaction confirmed');
 }
 
 async function wait(waitInMilliseconds) {
